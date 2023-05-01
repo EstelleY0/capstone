d2716d68989bec5571d0ad2a05918800a7b7e821
Estelle <107674294+EstelleY0@users.noreply.github.com>
2023-05-01 21:32:36 +0900

    Update serial.py

--- CodeByY/serial.py
+++ CodeByY/serial.py
@@ -8,6 +8,7 @@ pyserial = serial.Serial(port, baudrate)
 def judge(cam, us, lid):
     mid_p = camera_line(cam)
     ob_lid = lidar(lid)
+    us_collid = []
 
     # 장애물이 차선보다 먼저
     # 신호등이 최우선
@@ -15,17 +16,15 @@ def judge(cam, us, lid):
     # 초음파 센서 값 읽어오기 -일단은 매번 읽게 해둠. 근데 상황따라 수정 필
     if pyserial.readable():
         line = pyserial.readline()
-        us_data = line.decode()
-        # 초음파 센서 중 충돌거리 이하 감지한 센서 넘버링
-        us_collid = []
-        for i in range(6):
-            if us_data[i] == 1:
+        #  초음파 센서 중 충돌거리 이하 감지한 센서 넘버링
+        for i in list(line.decode()):
+            if i == 1:
                 us_collid.append(i)
 
     # 종합해서 상황판단
 
     # 상황판단 완료 후 각 상황에 대한 시그널 지정해서 보내기, 길면 안됨 시리얼 통신이라 지연존재
-    commend = "아두이노로 보내는 데이터"
+    commend = "아두이노로 보내는 명령"
     pyserial.write(commend.encode())
 
 def camera_line(cam):
