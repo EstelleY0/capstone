d18299c32cf1fe1f17009035e90e726fe9bc40ab
Estelle <107674294+EstelleY0@users.noreply.github.com>
2023-05-26 16:05:04 +0900

    Update serial.py

--- CodeByY/serial.py
+++ CodeByY/serial.py
@@ -12,40 +12,41 @@ def judge(cam, us, lid):
     us_collid = []
 
     # 장애물이 차선보다 먼저
-    # 신호등이 최우선
     
+    #차선 체킹
+    commnd = camera_line(cam)
     
     # 종합해서 상황판단
 
     # 상황판단 완료 후 각 상황에 대한 시그널 지정해서 보내기, 길면 안됨 시리얼 통신이라 지연존재
-    commend = "아두이노로 보내는 명령"
     pyserial.write(commend.encode())
 
-def US():
-    # 초음파 센서 값 읽어오기 -일단은 매번 읽게 해둠. 근데 상황따라 수정 필
-    if pyserial.readable():
-        line = pyserial.readline()
-        #  초음파 센서 중 충돌거리 이하 감지한 센서 넘버링
-        for i in list(line.decode()):
-            if i == 1:
-                us_collid.append(i)
+# def US():
+#     # 초음파 센서 값 읽어오기 -일단은 매번 읽게 해둠. 근데 상황따라 수정 필
+#     if pyserial.readable():
+#         line = pyserial.readline()
+#         #  초음파 센서 중 충돌거리 이하 감지한 센서 넘버링
+#         for i in list(line.decode()):
+#             if i == 1:
+#                 us_collid.append(i)
 
 
 def camera_line(cam):
     # From camera, get the V
     # N, find middle of the line
     middle_point = 0
+    command = None
     if cam < middle_point :
-        #turn right
+        command = 'right'
     elif cam > middle_point :
-        # turn left
+        command = 'left'
     else:
-        # straight
-    return None
+        command = 'straight'
+    return command
 
-def lidar(lid):
-    # From LIDAR, get data
-    # N, find the obstacle
-    # None, front, left, right, back
-    where = None
-    return where
+# def lidar(lid):
+#     # From LIDAR, get data
+#     # N, find the obstacle
+#     # None, front, left, right, back
+#     where = None
+#     return where
